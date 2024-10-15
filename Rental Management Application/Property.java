7b0a65c2316ce732a791ccd46fa813983ce22964
VyHoang245 <155942335+VyHoang245@users.noreply.github.com>
2024-10-15 10:14:00 +0700

    Create user and property

---
+++ Rental Management Application/Property.java
@@ -0,0 +1,12 @@
+public class Property {
+    public static void main(String[] args) {
+
+    }
+
+    public class property {
+        String PropertyID;
+        String Description;
+        String PropertyOwnerId;
+
+    }
+}
---
+++ Rental Management Application/User.java
@@ -0,0 +1,24 @@
+/**
+ * User
+ */
+public class User {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class user {
+        String userId;
+        String password;
+        String email;
+        String userType;
+
+        public void CreateUser(String userId, String password, String email, String userType) {
+
+        }
+
+        public void ResetPassword() {
+        }
+
+    }
+}
